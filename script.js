0b5d022af80dd328ea2b86f4767cce1299c0565a
bmk774 <mbekum9@gmail.com>
2026-09-03 17:44:32 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Force scroll to top on load
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+window.scrollTo(0, 0);
+
 // GSAP ScrollTrigger
 gsap.registerPlugin(ScrollTrigger);
 
